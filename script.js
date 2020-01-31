d2f6c288350405f98182989f9681b649077e2348
Prtmplish <57013725+Prtmplish@users.noreply.github.com>
2020-01-31 15:54:49 +0530

    This is the project on helping the budding programmer

---
+++ script.js
@@ -0,0 +1,26 @@
+//alert("connected")
+var myText = document.getElementById("textarea").value
+function show() {
+    let lbl = document.getElementById("prtm");
+    let empName = document.getElementById( "textarea" ).value;
+
+    lbl.innerText = empName;       
+    return 0;
+}
+
+var myText1 = document.getElementById("textarea1").value
+function show1() {
+    let lbl = document.getElementById("prtm1");
+    let empName = document.getElementById( "textarea1" ).value;
+
+    lbl.innerText = empName;       
+    return 0;
+}
+var myText2 = document.getElementById("textarea2").value
+function show2() {
+    let lbl = document.getElementById("prtm2");
+    let empName = document.getElementById( "textarea2" ).value;
+
+    lbl.innerText = empName;       
+    return 0;
+}
